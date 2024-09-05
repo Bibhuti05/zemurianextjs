5318759da7d43819599a714479afa3a6bf6369bd
Bibhuti05 <bibhuticodes@gmail.com>
2024-09-05 21:05:42 +0530

    fixed middleware

--- app/middleware.js
+++ app/middleware.js
@@ -1,28 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-const COOKIE_NAME = "bucket-marketing";
+export async function middleware(request) {
+  // Define the marketing buckets
+  const MARKETING_BUCKETS = ["original", "a", "b"];
 
-// Choose a random bucket
-// Optional: contact a 3rd party service to get the user's bucket
-const MARKETING_BUCKETS = ["original", "a", "b"];
-const getBucket = () =>
-  MARKETING_BUCKETS[Math.floor(Math.random() * MARKETING_BUCKETS.length)];
+  if (request.url.includes("/marketing")) {
+    // Function to get a random bucket
+    const getBucket = () => {
+      return MARKETING_BUCKETS[
+        Math.floor(Math.random() * MARKETING_BUCKETS.length)
+      ];
+    };
 
-export function middleware(req) {
-  console.log("running middleware");
-  // Get the bucket cookie
-  const bucket = req.cookies[COOKIE_NAME] || getBucket();
-  // Proxy to the appropriate variant
-
-  const absoluteUrl = `https://localhost:3000/marketing/${bucket}`;
-  console.log(req.nextUrl);
-  const res = NextResponse.rewrite(absoluteUrl);
-
-  // Add the bucket to cookies if it's not there
-  // Add the bucket to cookies if it's not there
-  if (!req.cookies[COOKIE_NAME]) {
-    res.cookies.set(COOKIE_NAME, bucket); // Use res.cookies.set() to set the cookie
+    // Redirect to a random marketing bucket
+    return NextResponse.redirect(
+      new URL(`marketing/${getBucket()}`, request.url)
+    );
   }
 
-  return res;
+  // Allow the request to continue if no conditions are met
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/marketing"],
+};
